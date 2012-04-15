82a83fd98cadf9e1132cb9da937ed16f47029abb
ceugster <christian.eugster@gmx.net>
2012-04-15 18:02:21 +0200

    simplified siard test case.

--- ch.kostceco.siard.impl.1.0.test/src/ch/kostceco/siard/impl/test/SiardTestCase.java
+++ ch.kostceco.siard.impl.1.0.test/src/ch/kostceco/siard/impl/test/SiardTestCase.java
@@ -19,32 +19,42 @@ import ch.kostceco.siard.impl.internal.Activator;
 public class SiardTestCase
 {
 	private ServiceTracker<SiardService, SiardService> tracker;
+
+	private File file;
+
+	private SiardService service;
 	
 	@Before
 	public void setUp() throws Exception
 	{
+		String path = "U:/Incubator Projekte/SIARD.val/Test Data/gebaeudeversicherung.siard";
+		file = new File(path);
+
 		tracker = new ServiceTracker<SiardService, SiardService>(Activator.getContext(), SiardService.class, null);
 		tracker.open();
+
+		service = tracker.getService();
 	}
 
 	@After
 	public void tearDown() throws Exception
 	{
+		service = null;
 		tracker.close();
+	}
+
+	@Test
+	public void testDirectoryStructure()
+	{
+		Assert.assertNotNull(service);
+
+		IStatus<? extends IAction<? extends IMessage>, ? extends IMessage> status = service.checkDirectoryStructure(file);
+		Assert.assertTrue(status.isOK());
 	}
 
 	@Test
 	public void testValidateHeaderMetadataXsd()
 	{
-		SiardService service = tracker.getService();
-		Assert.assertNotNull(service);
-
-		String path = "U:/Incubator Projekte/SIARD.val/Test Data/gebaeudeversicherung.siard";
-		File file = new File(path);
-
-		IStatus<? extends IAction<? extends IMessage>, ? extends IMessage> status = service.checkDirectoryStructure(file);
-		Assert.assertTrue(status.isOK());
-
 		try
 		{
 			Assert.assertTrue(service.validateHeaderMetadataXsd(file));
